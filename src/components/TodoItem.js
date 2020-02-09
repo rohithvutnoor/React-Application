7b00c0e735733a6ccd2d1d032225775be93fedf0
rohithvutnoor <rohithvutnoor@gmail.com>
2020-02-09 10:25:18 +0530

    Adding state to the components

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
 class TodoItem extends React.Component {
+    constructor(){
+        super()
+    }
     render() {
         return (
             <div className="todoitem">
                 <label>
-                    <input type="checkbox" checked={this.props.item.status} />
+                    <input type="checkbox" checked={this.props.item.status} 
+                        onChange={(event)=>this.props.handleChange(this.props.item.id)}
+                    />
                     <span>{this.props.item.title}</span>
                 </label>
             </div>
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,13 +3,32 @@ import TodoItem from './TodoItem'
 import data from './itemsList.json'
 
 class Todos extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: data
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.status = !todo.status
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
     render() {
-        const items = data.map(item => {
+        const items = this.state.todos.map(item => {
             return (
-                <TodoItem item={item} />
+                <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
             );
         });
-
         return (
             <div>
                 {items}
